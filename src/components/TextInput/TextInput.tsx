645eb09df98d368b285e3fa226234c9b3e039b0b
Brad Fisher <bradfisher95dev@gmail.com>
2023-10-02 16:46:40 +0100

    TextInput - finished a first draft of the TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import './TextInput.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export interface TextInputProps {
   label: string;
   placeholder: string;
   showIcon: boolean;
+  inputVisible: boolean;
 }
 
 const TextInput = ({
   label = 'label',
   placeholder = 'placeholder',
-  showIcon = false
+  showIcon = false,
+  inputVisible = true
 }: TextInputProps) => {
   return (
     <div className="text-input">
       <label>{label}</label>
       <input placeholder={placeholder} />
-      {showIcon ? <FontAwesomeIcon icon={faEye} className="icon" /> : null}
+      {showIcon ? (
+        inputVisible ? (
+          <FontAwesomeIcon icon={faEye} className="icon" />
+        ) : (
+          <FontAwesomeIcon icon={faEyeSlash} className="icon" />
+        )
+      ) : null}
     </div>
   );
 };
