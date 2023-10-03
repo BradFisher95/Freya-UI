d3443493a2cfff2d9a963a7a175b8a3b523f34bd
Brad Fisher <bradfisher95dev@gmail.com>
2023-10-03 18:11:32 +0100

    Checkbox - releasing checkbox version 1.0.12

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Button from './components/Button';
 import TextInput from './components/TextInput';
+import Checkbox from './components/Checkbox';
 
-export { Button, TextInput };
+export { Button, TextInput, Checkbox };
